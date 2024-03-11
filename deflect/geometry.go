39bf9580591d851de84693dec20cea9cd199667d
Lukas Böger <mail@lboeger.de>
2024-03-11 10:38:44 +0000

    Extract function for 2d sine/cosine from nodes
    To be shared between 2d truss and beam implementations.

--- deflect/geometry.go
+++ deflect/geometry.go
@@ -8,3 +8,13 @@ func length(n0, n1 *Node) float64 {
 	line := r3.Sub(r3.Vec{X: n1.X, Y: n1.Y, Z: n1.Z}, r3.Vec{X: n0.X, Y: n0.Y, Z: n0.Z})
 	return r3.Norm(line)
 }
+
+func sineCosine2d(n0, n1 *Node) (sin, cos float64) {
+	// Gonum's spatial/r2 package doesn't seem to have a way to create a matrix representation of a 2d
+	// rotation (only for 3d), so we do it manually.
+	line := r3.Sub(r3.Vec{X: n1.X, Y: n1.Y, Z: n1.Z}, r3.Vec{X: n0.X, Y: n0.Y, Z: n0.Z})
+	cos = r3.Cos(line, r3.Vec{X: 1.0, Y: 0.0, Z: 0.0})
+	sin = r3.Cos(line, r3.Vec{X: 0.0, Y: 0.0, Z: 1.0})
+
+	return sin, cos
+}
--- deflect/geometry_test.go
+++ deflect/geometry_test.go
@@ -25,3 +25,32 @@ func TestNonZeroLength(t *testing.T) {
 		t.Errorf("Expected length %v, got %v", expected, actual)
 	}
 }
+
+func TestSineCosine2d(t *testing.T) {
+	cases := []struct{ x1, z1, angle float64 }{
+		{x1: 0, z1: 0, angle: 0},
+		{x1: 1, z1: 0, angle: 0},
+		{x1: 1, z1: 1, angle: 45},
+		{x1: 0, z1: 1, angle: 90},
+		{x1: -1, z1: 1, angle: 135},
+		{x1: -1, z1: 0, angle: 180},
+		{x1: -1, z1: -1, angle: 225},
+		{x1: 0, z1: -1, angle: 270},
+		{x1: math.Cos(1.23456), z1: math.Sin(1.23456), angle: 1.23456 * 180.0 / math.Pi},
+	}
+
+	for _, test := range cases {
+		n0 := &Node{ID: "A", X: 0.0, Y: 0.0, Z: 0.0}
+		n1 := &Node{ID: "B", X: test.x1, Y: 0.0, Z: test.z1}
+		sin, cos := sineCosine2d(n0, n1)
+		alpha := math.Pi * test.angle / 180.0
+
+		if expected := math.Sin(alpha); math.Abs(sin-expected) > 1.e-10 {
+			t.Errorf("Expected angle %v with sine %v, got sine %v", test.angle, expected, sin)
+		}
+
+		if expected := math.Cos(alpha); math.Abs(cos-expected) > 1.e-10 {
+			t.Errorf("Expected angle %v with cosine %v, got cosine %v", test.angle, expected, cos)
+		}
+	}
+}
